0ec3b9f63234b66997b9a3b3b276b567d779da04
nikjft <nikjft@users.noreply.github.com>
2025-02-15 15:26:09 -0700

    Minimal sw

--- sw.js
+++ sw.js
@@ -1,27 +1,5 @@
-// sw.js
-self.addEventListener('install', function(event) {
-    // Cache essential files for offline use
-    event.waitUntil(
-        caches.open('my-pwa-cache-v1').then(function(cache) {
-            return cache.addAll([
-                '/', // Cache the root URL
-                '/index.html',
-                '/main.js', // Cache your JavaScript file
-                '/styles.css', // Cache your CSS file
-                '/fontawesome.min.css',  // If you have a local copy                '/icon-512x512.png'
-                // Add any other essential files here
-            ]);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  // This is a minimal service worker.  It doesn't do any caching.
+  // It's just enough to satisfy the installability requirements.
+  event.respondWith(fetch(event.request));
 });
-
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-
